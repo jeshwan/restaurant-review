f659f26fa6234a127cf24888544888d19ab05c34
Akhilesh Ramkelowan <akhilesh.ramkelowan@sdworx.com>
2023-12-15 14:42:43 +0400

    feat: add review and rating model

--- Lib/models/Review.cs
+++ Lib/models/Review.cs
@@ -1,6 +1,13 @@
 ﻿namespace Lib.models
 {
-    public enum Review 
+    public class Review
     {
+        public int ReviewId { get; set; }
+        public int RestaurantId { get; set; }
+        public int ReviewerId { get; set; }
+        public string Comment { get; set; }
+        public Rating Rating { get; set; }
+        public DateTime Date { get; set; }
     }
 }
+
