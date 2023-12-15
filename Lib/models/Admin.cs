ba215123b373daa1c64283387ec17d4c0d83a49a
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2023-12-15 14:52:25 +0400

    admin updates

--- Lib/models/Admin.cs
+++ Lib/models/Admin.cs
@@ -1,6 +1,52 @@
-﻿namespace Lib.models
+﻿using Lib.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Admin
 {
-    public class Admin
+    public int AdminId { get; set; }
+    public string AdminName { get; set; }
+
+    // User and Review databases
+    private List<User> usersDatabase;
+    private List<Review> reviewsDatabase;
+
+    public Admin()
     {
+        // Initialize databases (replace this with your actual data source or database)
+        usersDatabase = new List<User>();
+        reviewsDatabase = new List<Review>();
+    }
+
+    public void AddNewUser(string userName)
+    {
+        if (string.IsNullOrEmpty(userName))
+        {
+            Console.WriteLine("User name cannot be null or empty.");
+            return;
+        }
+
+        int userId = usersDatabase.Count + 1;
+        usersDatabase.Add(new User { UserId = userId, UserName = userName });
+        Console.WriteLine($"User '{userName}' with ID {userId} added successfully.");
+    }
+
+    public User SearchUser(string userName)
+    {
+        return usersDatabase.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void AddReview(int userId, int rating, string comment)
+    {
+        if (rating < 1 || rating > 5)
+        {
+            Console.WriteLine("Rating must be between 1 and 5.");
+            return;
+        }
+
+        int reviewId = reviewsDatabase.Count + 1;
+        reviewsDatabase.Add(new Review { ReviewId = reviewId, Rating = rating, Comment = comment });
+        Console.WriteLine($"Review with ID {reviewId} added successfully.");
     }
 }
